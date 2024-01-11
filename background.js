869e131108ee6063a8a6386bc90914578d5d9a85
Michael Buen <ienablemuch@gmail.com>
2024-01-11 09:16:21 +0800

    toolbar icon

--- background.js
+++ background.js
@@ -8,21 +8,39 @@ chrome.commands.onCommand.addListener(command => {
     }
 });
 
-// chrome.action.onclicked
+
+
+chrome.action.onClicked.addListener(() => {
+    const isEdge = /Edg/i.test(navigator.userAgent);
+    const prefix = isEdge ? 'edge' : 'chrome';
+    createTab(`${prefix}://extensions/shortcuts#:~:text=NoSwitch`);
+});
 
 function tellTask(task) {
-    chrome.tabs.query({active: true, currentWindow: true }, ([theActiveTab]) => {
-        // console.log('theActiveTab', theActiveTab);
+    chrome.tabs.query({ }, tabs => {
+        console.log('tabs', tabs);
 
-        // eslint-disable-next-line no-undef
-        chrome.tabs.query({ }, tabs => {
-            console.log('tabs', tabs);
+        const activeTab = tabs.filter(e => e.active)?.[0];
 
-            const activeTab = tabs.filter(e => e.active)?.[0];
+        if (!activeTab) return;
 
-            if (!activeTab) return;
-
-            chrome.tabs.sendMessage(activeTab.id, { task });
-        });
+        chrome.tabs.sendMessage(activeTab.id, { task });
     });
 }
+
+
+async function createTab(url, useOpener) {
+    let openerTabId;
+
+    if (useOpener) {
+        try {
+            const tab = await chrome.tabs.getCurrent();
+            openerTabId = tab.id;
+        } catch (_e) {
+            // NOP
+        }
+    }
+
+    return await chrome.tabs.create({ url, openerTabId });
+}
+
